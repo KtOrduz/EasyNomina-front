8695abedd688d8632bb5656a404d6ad6f72b25aa
Stente3 <stentem0103@gmail.com>
2024-11-25 22:08:35 -0500

    Se añadio TailwindCSS y React-router al proyecto junto con una página 404

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import { createRoot } from "react-dom/client";
-import { App } from "./components/App";
 import "./index.css";
+import { HomePage } from "./pages/HomePage.jsx";
+import { NotFound } from "./pages/NotFound.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <NotFound />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />,
 );
